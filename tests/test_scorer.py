c7f4ffa8f8cc7e1799c5d1f4bbe26f2b441cae92
Loïc Grobol <loic.grobol@gmail.com>
2018-03-15 12:18:40 +0100

    add: basic test for MUC

--- tests/test_scorer.py
+++ tests/test_scorer.py
@@ -1,9 +1,19 @@
+import pytest
+
 from scorer import scorer  # noqa
 
 
-def test_trace():
-    K = [{'a', 'b', 'c'}, {'d', 'e', 'f', 'g'}]
-    R = [{'a', 'b'}, {'c', 'd'}, {'f', 'g', 'h', 'i'}]
+@pytest.fixture()
+def K():
+    return [{'a', 'b', 'c'}, {'d', 'e', 'f', 'g'}]
+
+
+@pytest.fixture()
+def R():
+    return [{'a', 'b'}, {'c', 'd'}, {'f', 'g', 'h', 'i'}]
+
+
+def test_trace(K, R):
     expected_K_wrt_R = [[{'a', 'b'}, {'c'}],
                         [{'d'}, {'f', 'g'}, {'e'}]]
     expected_R_wrt_K = [[{'a', 'b'}],
@@ -13,3 +23,10 @@ def test_trace():
     R_wrt_K = [list(scorer.trace(c, K)) for c in R]
     assert K_wrt_R == expected_K_wrt_R
     assert R_wrt_K == expected_R_wrt_K
+
+
+def test_muc(K, R):
+    R, P, F = scorer.muc(K, R)
+    assert R == pytest.approx(0.40)
+    assert P == pytest.approx(0.40)
+    assert F == pytest.approx(0.40)
