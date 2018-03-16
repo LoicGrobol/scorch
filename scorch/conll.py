22305e37945e3a52c746eba2068538271b60edf0
Loïc Grobol <loic.grobol@gmail.com>
2018-03-16 18:02:34 +0100

    add: cli for conll

--- scorch/conll.py
+++ scorch/conll.py
@@ -1,10 +1,33 @@
-'''Parse CoNLL-2012 files.'''
+#! /usr/bin/env python3
+r"""Convert CoNLL-2012 files to simple JSON.
 
+Usage:
+  conll <conll-file> [<out-dir>]
+
+Arguments:
+  <conll-file>  input file (CoNLL-2012 format), `-` for standard input
+  <out-dir>     directory for output [default: same as the input]
+
+Options:
+  -h, --help  Show this screen.
+
+Example:
+  `conll input.conll ./out/`
+"""
+
+__version__ = 'conll 0.0.0'
+
+import contextlib
+import json
+import pathlib
 import re
+import sys
 
 import typing as ty
 
 from collections import defaultdict
+
+from docopt import docopt
 
 
 def parse_block(lines: ty.Iterable[str], column=-1) -> ty.Dict[str, ty.List[ty.Tuple[str, str]]]:
@@ -100,3 +123,53 @@ def parse_file(lines: ty.Iterable[str],
                 yield (doc_name, doc_entities)
         else:
             buffer.append(l)
+
+
+# Thanks http://stackoverflow.com/a/17603000/760767
+@contextlib.contextmanager
+def smart_open(filename: str = None, mode: str = 'r', *args, **kwargs):
+    '''Open files and i/o streams transparently.'''
+    if filename == '-':
+        if 'r' in mode:
+            stream = sys.stdin
+        else:
+            stream = sys.stdout
+        if 'b' in mode:
+            fh = stream.buffer
+        else:
+            fh = stream
+    else:
+        fh = open(filename, mode, *args, **kwargs)
+
+    try:
+        yield fh
+    finally:
+        try:
+            fh.close()
+        except AttributeError:
+            pass
+
+
+def main_entry_point(argv=None):
+    arguments = docopt(__doc__, version=__version__, argv=argv)
+    # Since there are no support for default positional arguments in
+    # docopt yet. Might be useful for complex default values, too
+    if arguments['<out-dir>'] is None:
+        if arguments['<conll-file>'] == '-':
+            arguments['<out-dir>'] = pathlib.Path.cwd()
+        else:
+            arguments['<out-dir>'] = pathlib.Path(arguments['<conll-file>']).parent
+    else:
+        arguments['<out-dir>'] = pathlib.Path(arguments['<out-dir>'])
+
+    with smart_open(arguments['<conll-file>']) as in_stream:
+        documents = list(parse_file((l.strip() for l in in_stream)))
+
+    for name, entities in documents:
+        out_path = arguments['<out-dir>']/f'{name}.json'
+        with out_path.open('w') as out_stream:
+            json.dump(entities, out_stream)
+
+
+if __name__ == '__main__':
+    sys.exit(main_entry_point())
