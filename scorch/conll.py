a9a5a59060b1c61c73a96ea2a8034af93a6f295e
Loïc Grobol <loic.grobol@gmail.com>
2018-03-16 16:08:39 +0100

    add: CoNLL file parsing

--- scorch/conll.py
+++ scorch/conll.py
@@ -6,25 +6,97 @@ import typing as ty
 
 from collections import defaultdict
 
-Span = ty.NewType('Span', ty.Tuple[ty.Hashable, ty.Hashable])
 
-
-def parse_block(lines: ty.Iterable[str], column=-1) -> ty.Dict[str, ty.List[Span]]:
+def parse_block(lines: ty.Iterable[str], column=-1) -> ty.Dict[str, ty.List[ty.Tuple[str, str]]]:
+    '''
+    Parse a bloc (≈sentence) in the CoNLL format, return entities as mappings
+    ```
+    entity_id: [(mention_start, mention_end), …]
+    ```
+    '''
     entities = defaultdict(list)
     dangling = defaultdict(list)
-    for l in lines:
+    for i, l in enumerate(lines):
         row = l.split('\t')
 
-        row_n = row[2]
-        coref = row[column]
+        try:
+            row_n = row[2]
+            coref = row[column]
+        except IndexError:
+            raise ValueError(f'Badly formatted line: {l!r}')
         if coref == '-':
             continue
 
-        for m in re.finditer(r'\(([^()]+)', coref):
+        for m in re.finditer(r'\((\d+)', coref):
             dangling[m.group(1)].append(row_n)
 
-        for m in re.finditer(r'([^()])\)', coref):
-            entities[m.group(1)].append((dangling[m.group(1)].pop(), row_n))
+        for m in re.finditer(r'(\d+)\)', coref):
+            try:
+                entities[m.group(1)].append((dangling[m.group(1)].pop(), row_n))
+            except IndexError:
+                raise ValueError(f'Unbalanced parentheses at line {i}: {l!r}')
     if any(dangling.values()):
-        raise ValueError(f'Dangling mentions: {[e for e, v in dangling.items() if e]}')
-    return entities
+        raise ValueError(f'Dangling mentions at line {i}: {[e for e, v in dangling.items() if e]}')
+    return dict(entities)
+
+
+def split_blocks(lines: ty.Iterable[str]) -> ty.Iterable[ty.List[str]]:
+    '''Split blocks in a CoNLL document.'''
+    buffer = []
+    for l in lines:
+        if not l or l.isspace():
+            yield buffer
+            buffer = []
+        else:
+            buffer.append(l)
+    if buffer:
+        yield buffer
+
+
+def parse_document(lines: ty.Iterable[str], column=-1
+                   ) -> ty.Dict[str, ty.List[ty.Tuple[int, str, str]]]:
+    '''
+    Parse a document in the CoNLL format, return entities as mappings
+    ```
+    entity_id: [(block_number, mention_start, mention_end), …]
+    ```
+    '''
+    entities = defaultdict(list)
+    for i, block in enumerate(split_blocks(lines)):
+        try:
+            block_entities = parse_block(block)
+        except ValueError as e:
+            raise ValueError(
+                'Parse error in block {i}:\n{e}\n{block}'.format(i=i, e=e,
+                                                                block="\n".join(block)))
+        # Merge this block entities
+        for e, mentions in block_entities.items():
+            entities[e].extend([(i, start, end) for start, end in mentions])
+    return dict(entities)
+
+
+def parse_file(lines: ty.Iterable[str],
+               column=-1,
+              ) -> ty.Iterable[ty.Tuple[str, ty.Dict[str, ty.List[ty.Tuple[int, str, str]]]]]:
+    '''Parse a CoNLL file, return document as tuples `(document_name, entities)` where
+       entities are mappings
+       ```
+       entity_id: [(block_number, mention_start, mention_end), …]
+       ```'''
+    buffer = []
+    for l in lines:
+        if l.startswith('#'):
+            m = re.match(r'#\s*begin document \((.*?)\);(\s*part (.*))?', l)
+            if m:
+                if m.group(2):
+                    doc_name = f'{m.group(1)}-{m.group(3)}'
+                else:
+                    doc_name = m.group(1)
+                continue
+            m = re.match(r'#\s*end document', l)
+            if m:
+                doc_entities = parse_document(buffer, column)
+                buffer = []
+                yield (doc_name, doc_entities)
+        else:
+            buffer.append(l)
--- tests/test_conll.py
+++ tests/test_conll.py
@@ -1,6 +1,4 @@
 '''Unit tests for `conll.py`.'''
-import textwrap
-
 import pytest
 
 from scorch import conll  # noqa
@@ -11,6 +9,22 @@ def block():
     return [
         'test1	0	0	a1	(0',
         'test1	0	1	a2	0)',
+        'test1	0	2	jnk	-',
+        'test1	0	3	b1	(1',
+        'test1	0	4	b2	-',
+        'test1	0	5	b3	-',
+        'test1	0	6	b4	1)',
+        'test1	0	7	jnk	-',
+        'test1	0	8	.	-',
+        'test1	0	9	jun	(0)',
+    ]
+
+
+@pytest.fixture()
+def document():
+    return [
+        'test1	0	0	a1	(0',
+        'test1	0	1	a2	0)',
         'test1	0	2	junk	-',
         'test1	0	3	b1	(1',
         'test1	0	4	b2	-',
@@ -18,12 +32,110 @@ def block():
         'test1	0	6	b4	1)',
         'test1	0	7	jnk	-',
         'test1	0	8	.	-',
+        'test1	0	9	jun	(0)',
+        '',
+        'test2	0	0	c	(1)',
+        'test2	0	1	jnk	-',
+        'test2	0	2	d1	(2',
+        'test2	0	3	d2	2)',
+        'test2	0	4	jnk	-',
+        'test2	0	5	e	(2)',
+        'test2	0	6	jnk	-',
+        'test2	0	7	f1	(2',
+        'test2	0	8	f2	-',
+        'test2	0	9	f3	2)',
+        'test2	0	10	.	-',
     ]
 
 
+@pytest.fixture()
+def conll_file():
+    return [
+        '#begin document (Testcase1);',
+        'test1	0	0	a1	(0',
+        'test1	0	1	a2	0)',
+        'test1	0	2	junk	-',
+        'test1	0	3	b1	(1',
+        'test1	0	4	b2	-',
+        'test1	0	5	b3	-',
+        'test1	0	6	b4	1)',
+        'test1	0	7	jnk	-',
+        'test1	0	8	.	-',
+        'test1	0	9	jun	(0)',
+        '',
+        'test2	0	0	c	(1)',
+        'test2	0	1	jnk	-',
+        'test2	0	2	d1	(2',
+        'test2	0	3	d2	2)',
+        'test2	0	4	jnk	-',
+        'test2	0	5	e	(2)',
+        'test2	0	6	jnk	-',
+        'test2	0	7	f1	(2',
+        'test2	0	8	f2	-',
+        'test2	0	9	f3	2)',
+        'test2	0	10	.	-',
+        '#end document',
+        '#begin document (Testcase2); part 000',
+        'test1	0	0	a1	(0',
+        'test1	0	1	a2	0)',
+        'test1	0	2	jnk	-',
+        'test1	0	3	b1	(1',
+        'test1	0	4	b2	-',
+        'test1	0	5	b3	-',
+        'test1	0	6	b4	1)',
+        'test1	0	7	jnk	-',
+        'test1	0	8	.	-',
+        'test1	0	9	jun	(0)',
+        '# end document',
+    ]
+
 
 def test_parse_block(block):
-    expected_entities = {'0': [('0', '1')],
+    expected_entities = {'0': [('0', '1'), ('9', '9')],
                          '1': [('3', '6')]}
     entities = conll.parse_block(block)
     assert entities == expected_entities
+
+
+def test_parse_document(document):
+    expected_entities = {
+        '0': [(0, '0', '1'), (0, '9', '9')],
+        '1': [
+            (0, '3', '6'),
+            (1, '0', '0'),
+        ],
+        '2': [
+            (1, '2', '3'),
+            (1, '5', '5'),
+            (1, '7', '9'),
+        ]
+    }
+    entities = conll.parse_document(document)
+    assert entities == expected_entities
+
+
+def test_parse_file(conll_file):
+    expected_documents = [
+        (
+            'Testcase1',
+            {
+                '0': [(0, '0', '1'), (0, '9', '9')],
+                '1': [
+                    (0, '3', '6'),
+                    (1, '0', '0'),
+                ],
+                '2': [
+                    (1, '2', '3'),
+                    (1, '5', '5'),
+                    (1, '7', '9'),
+                ],
+            }
+        ),
+        (
+            'Testcase2-000',
+            {'0': [(0, '0', '1'), (0, '9', '9')],
+             '1': [(0, '3', '6')]}
+        ),
+    ]
+    documents = conll.parse_file(conll_file)
+    assert list(documents) == expected_documents
