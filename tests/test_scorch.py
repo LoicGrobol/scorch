3beda3f04ba4a881a9956b07e4b28056413fb562
Loïc Grobol <loic.grobol@gmail.com>
2018-03-22 14:04:02 +0100

    add: test for scorch main

--- tests/test_scorch.py
+++ tests/test_scorch.py
@@ -1,4 +1,5 @@
 '''Unit tests for `scorch.py`.'''
+import pathlib
 import unittest
 
 import pytest
@@ -19,9 +20,36 @@ def links():
     ]
 
 
+@pytest.fixture()
+def gold_file(request):
+    test_file = pathlib.Path(request.module.__file__)
+    return str(test_file.resolve().parent/'fixtures'/'json'/'gold.json')
+
+
+@pytest.fixture()
+def sys_file(request):
+    test_file = pathlib.Path(request.module.__file__)
+    return str(test_file.resolve().parent/'fixtures'/'json'/'sys.json')
+
+
+@pytest.fixture()
+def out_file(request):
+    test_file = pathlib.Path(request.module.__file__)
+    return str(test_file.resolve().parent/'fixtures'/'json'/'out.txt')
+
+
 def test_greedy_clustering(links):
     case = unittest.TestCase()
     expected_clusters = [{1, 2, 3}, {4, 5, 6}, {7, 8}]
     clusters = scorch.greedy_clustering(links)
     print(clusters)
     case.assertCountEqual(clusters, expected_clusters)
+
+
+def test_process_files(gold_file, sys_file, out_file, tmpdir):
+    with open(out_file) as stream:
+        expected_output = stream.read()
+    scorch.main_entry_point([gold_file, sys_file, str(tmpdir.join('out.txt'))])
+    with tmpdir.join('out.txt').open() as stream:
+        output = stream.read()
+    assert output == expected_output
