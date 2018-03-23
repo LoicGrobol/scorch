a4a1b815e5170882043635ea5ea6afe7aeb6055b
Loïc Grobol <loic.grobol@gmail.com>
2018-03-23 12:20:27 +0100

    add: (auto generated : consistency only!) test for process_dir

--- tests/test_scorch.py
+++ tests/test_scorch.py
@@ -35,18 +35,41 @@ def sys_file(request):
 @pytest.fixture()
 def out_file(request):
     test_file = pathlib.Path(request.module.__file__)
-    return (test_file.resolve().parent/'fixtures'/'json'/'single'/'out.txt').open()
+    return (test_file.resolve().parent/'fixtures'/'json'/'single'/'out.txt').open().read()
+
+
+@pytest.fixture()
+def gold_dir(request):
+    test_file = pathlib.Path(request.module.__file__)
+    return (test_file.resolve().parent/'fixtures'/'json'/'multiple'/'gold')
+
+
+@pytest.fixture()
+def sys_dir(request):
+    test_file = pathlib.Path(request.module.__file__)
+    return (test_file.resolve().parent/'fixtures'/'json'/'multiple'/'sys')
+
+
+@pytest.fixture()
+def out_file_multiple(request):
+    test_file = pathlib.Path(request.module.__file__)
+    return (test_file.resolve().parent/'fixtures'/'json'/'multiple'/'out.txt').open().read()
 
 
 def test_greedy_clustering(links):
     case = unittest.TestCase()
     expected_clusters = [{1, 2, 3}, {4, 5, 6}, {7, 8}]
     clusters = scorch.greedy_clustering(links)
-    print(clusters)
     case.assertCountEqual(clusters, expected_clusters)
 
 
 def test_process_files(gold_file, sys_file, out_file):
-    expected_output = out_file.read()
+    expected_output = out_file
     output = ''.join(scorch.process_files(gold_file, sys_file))
+    assert output == expected_output
+
+
+def test_process_dirs(gold_dir, sys_dir, out_file_multiple):
+    expected_output = out_file_multiple
+    output = ''.join(scorch.process_dirs(gold_dir, sys_dir))
     assert output == expected_output
