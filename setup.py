89805b1d9116dbce67a3ce8321d7795d7f1070d2
Loïc Grobol <loic.grobol@gmail.com>
2018-04-03 20:34:35 +0200

    dist: move more metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,15 +1,6 @@
 import setuptools
 
 setuptools.setup(
-    name='scorch',
-    description='Alternative scorer for the CoNLL-2011/2012 shared tasks on coreference resolution.',
-    url='https://github.com/LoicGrobol/scorch',
-    author='Loïc Grobol',
-    author_email='loic.grobol@gmail.com',
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-    ],
     packages=['scorch'],
     install_requires=[
         'docopt',
