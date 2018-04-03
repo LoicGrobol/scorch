33b2d4461b8dc629bbcde6b2b7b4e505bc89d722
Loïc Grobol <loic.grobol@gmail.com>
2018-04-03 21:34:06 +0200

    switch all the metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
 import setuptools
 
-setuptools.setup(
-    packages=['scorch'],
-    install_requires=[
-        'docopt',
-        'scipy',
-        'numpy',
-    ],
-    extras_require={
-        'test': ['pytest'],
-    },
-    entry_points={
-    'console_scripts': [
-        'scorch=scorch.scorch:main_entry_point',
-    ],
-},
-)
+setuptools.setup()
